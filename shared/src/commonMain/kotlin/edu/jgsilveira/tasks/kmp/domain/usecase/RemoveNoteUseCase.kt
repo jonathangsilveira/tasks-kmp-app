906d4c71569f4e52ae52948e7a194f98fbb51434
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2025-06-14 17:39:21 -0300

    Move repository to another dir

--- shared/src/commonMain/kotlin/edu/jgsilveira/tasks/kmp/domain/usecase/RemoveNoteUseCase.kt
+++ shared/src/commonMain/kotlin/edu/jgsilveira/tasks/kmp/domain/usecase/RemoveNoteUseCase.kt
@@ -1,6 +1,6 @@
 package edu.jgsilveira.tasks.kmp.domain.usecase
 
-import edu.jgsilveira.tasks.kmp.data.NoteRepository
+import edu.jgsilveira.tasks.kmp.data.repository.NoteRepository
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
