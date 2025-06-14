064e6b3c18ee4c61cdb3198dbfb6b0c3f4cdb812
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2025-06-14 17:42:46 -0300

    Add function to get note by id

--- room-database/src/commonMain/kotlin/edu/jgsilveira/tasks/kmp/roomdb/dao/NoteDao.kt
+++ room-database/src/commonMain/kotlin/edu/jgsilveira/tasks/kmp/roomdb/dao/NoteDao.kt
@@ -8,15 +8,18 @@ import edu.jgsilveira.tasks.kmp.roomdb.entity.NoteEntity
 @Dao
 abstract class NoteDao {
 
-    @Query("SELECT * FROM note ORDER BY is_done ASC, updated_at_millis DESC")
+    @Query("SELECT * FROM notes ORDER BY is_done ASC, updated_at_millis DESC")
     abstract suspend fun getAllNotes(): List<NoteEntity>
+
+    @Query("SELECT * FROM notes WHERE id = :id")
+    abstract suspend fun getNoteById(id: Long): NoteEntity?
 
     @Upsert
     abstract suspend fun upsertNote(note: NoteEntity): Long
 
-    @Query("DELETE FROM note WHERE 1 = 1")
+    @Query("DELETE FROM notes WHERE 1 = 1")
     abstract suspend fun clearChecklists()
 
-    @Query("DELETE FROM note WHERE id in (:id)")
+    @Query("DELETE FROM notes WHERE id in (:id)")
     abstract suspend fun deleteNoteById(vararg id: Long)
 }
