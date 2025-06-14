0a4af015d0ae351133652210ea896b23dd34f800
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2025-06-14 17:40:42 -0300

    Change DI to new implementations

--- shared/src/commonMain/kotlin/edu/jgsilveira/tasks/kmp/di/domainKoinModule.kt
+++ shared/src/commonMain/kotlin/edu/jgsilveira/tasks/kmp/di/domainKoinModule.kt
@@ -1,7 +1,9 @@
 package edu.jgsilveira.tasks.kmp.di
 
-import edu.jgsilveira.tasks.kmp.data.InMemoryNoteRepository
-import edu.jgsilveira.tasks.kmp.data.NoteRepository
+import edu.jgsilveira.tasks.kmp.data.datasource.NoteLocalDataSource
+import edu.jgsilveira.tasks.kmp.data.datasource.NoteLocalDataSourceImpl
+import edu.jgsilveira.tasks.kmp.data.repository.NoteRepository
+import edu.jgsilveira.tasks.kmp.data.repository.NoteRepositoryImpl
 import edu.jgsilveira.tasks.kmp.domain.usecase.ChangeNoteUseCase
 import edu.jgsilveira.tasks.kmp.domain.usecase.GetAllNotesUseCase
 import edu.jgsilveira.tasks.kmp.domain.usecase.GetNoteByIdUseCase
@@ -9,12 +11,16 @@ import edu.jgsilveira.tasks.kmp.domain.usecase.NewNoteUseCase
 import edu.jgsilveira.tasks.kmp.domain.usecase.RemoveNoteUseCase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
-import org.koin.core.module.dsl.singleOf
-import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val domainKoinModule = module {
-    singleOf(::InMemoryNoteRepository) bind NoteRepository::class
+    factory<NoteLocalDataSource> {
+        NoteLocalDataSourceImpl(dao = get())
+    }
+
+    factory<NoteRepository> {
+        NoteRepositoryImpl(localDataSource = get())
+    }
 
     factory {
         GetNoteByIdUseCase(
