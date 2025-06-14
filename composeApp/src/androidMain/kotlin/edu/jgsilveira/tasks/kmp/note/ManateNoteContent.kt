c08e50911778fc88f58100becaedd78bf0d5ee99
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2025-06-14 20:16:26 -0300

    Remove unused composable

--- composeApp/src/androidMain/kotlin/edu/jgsilveira/tasks/kmp/note/ManateNoteContent.kt
+++
@@ -1,9 +0,0 @@
-package edu.jgsilveira.tasks.kmp.note
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-
-@Composable
-fun ManageNoteContent(modifier: Modifier) {
-
-}
