5e9637acd90d4bd831d047eda1d8e7036b2cc4dc
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2025-07-08 07:56:20 -0300

    Add and apply ktor client loggin

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -39,6 +39,7 @@ kotlin {
 
             implementation(libs.ktor.client.core)
             implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.client.loggin)
             implementation(libs.ktor.serialization.kotlinx.json)
         }
 
