1ad3454a6d05a16b757fc97b9f233c4f8cd30410
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2025-07-20 17:22:40 -0300

    Add UI view model

---
+++ composeApp/src/androidMain/kotlin/edu/jgsilveira/tasks/kmp/core/arch/viewmodel/UIEffect.kt
@@ -0,0 +1,3 @@
+package edu.jgsilveira.tasks.kmp.core.arch.viewmodel
+
+internal interface UIEffect
---
+++ composeApp/src/androidMain/kotlin/edu/jgsilveira/tasks/kmp/core/arch/viewmodel/UIViewModel.kt
@@ -0,0 +1,40 @@
+package edu.jgsilveira.tasks.kmp.core.arch.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+internal abstract class UIViewModel<STATE: UIState, EFFECT: UIEffect, ACTION: UIState>(
+    initialState: STATE
+) : ViewModel() {
+
+    private val mutableState = MutableStateFlow(initialState)
+    val uiState: StateFlow<STATE>
+        get() = mutableState
+
+    private val uiEffectChannel = Channel<EFFECT>(
+        capacity = Channel.BUFFERED,
+        onBufferOverflow = BufferOverflow.SUSPEND
+    )
+    val uiEffects: Flow<EFFECT>
+        get() = uiEffectChannel.receiveAsFlow()
+
+    abstract fun dispatchAction(uiAction: ACTION)
+
+    protected fun setState(reducer: (currentState: STATE) -> STATE) {
+        mutableState.update(reducer)
+    }
+
+    protected fun sendEffect(uiEffect: EFFECT) {
+        viewModelScope.launch {
+            uiEffectChannel.trySend(uiEffect)
+        }
+    }
+}
